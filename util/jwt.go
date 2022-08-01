9fe0f0754044b071acce29ee6590e5324edaa6cd
John Rockwell <john@llewkcor.com>
2022-08-01 15:58:38 -0700

    main: re-implemented ParseToken and GenerateToken functions

---
+++ util/jwt.go
@@ -0,0 +1,47 @@
+package util
+
+import (
+	"ares/config"
+	"fmt"
+	"github.com/golang-jwt/jwt/v4"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func ParseToken(tokenString string) (primitive.ObjectID, error) {
+	var objectId primitive.ObjectID
+
+	conf := config.Get()
+	secret := []byte(conf.Security.JWT)
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return secret, nil
+	})
+
+	if err != nil {
+		return objectId, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		idString := claims["accountId"].(string)
+		objectId, objectIdErr := primitive.ObjectIDFromHex(idString)
+		return objectId, objectIdErr
+	}
+
+	return objectId, fmt.Errorf("bad token on %v", tokenString)
+}
+
+func GenerateToken(accountId string) (string, error) {
+	conf := config.Get()
+	secret := []byte(conf.Security.JWT)
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"accountId": accountId,
+	})
+
+	tokenString, err := token.SignedString(secret)
+	return tokenString, err
+}
