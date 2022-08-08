5938d52344ec0c582c96873d95425c5227f18dc2
John Rockwell <john@llewkcor.com>
2022-08-07 20:02:31 -0700

    main: implemented deleted models for posts

--- model/deleted.go
+++ model/deleted.go
@@ -8,6 +8,24 @@ import (
 
 type DeletedAccount struct {
 	ID        primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-	Account   Account            `json:"account,omitempty" bson:"account,omitempty" binding:"required"`
-	RemovalAt time.Time          `json:"removalAt,omitempty" bson:"removalAt,omitempty" binding:"required"`
+	Account   Account            `json:"account" bson:"account" binding:"required"`
+	RemovalAt time.Time          `json:"removalAt" bson:"removalAt" binding:"required"`
+}
+
+type DeletedSession struct {
+	ID        primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Session   Session            `json:"session" bson:"session" binding:"required"`
+	RemovalAt time.Time          `json:"removalAt" bson:"removalAt" binding:"required"`
+}
+
+type DeletedPost struct {
+	ID        primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Post      Post               `json:"post" bson:"post" binding:"required"`
+	RemovalAt time.Time          `json:"removalAt" bson:"removalAt" binding:"required"`
+}
+
+type DeletedComment struct {
+	ID        primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Comment   Comment            `json:"comment" bson:"comment" binding:"required"`
+	RemovalAt time.Time          `json:"removalAt" bson:"removalAt" binding:"required"`
 }
