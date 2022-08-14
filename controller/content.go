8183522d9b43f6a71626a66b5afca2f619e88a08
John Rockwell <john@llewkcor.com>
2022-08-14 10:20:46 -0700

    main: implemented GetPostByID and CreatePost handlers

--- controller/content.go
+++ controller/content.go
@@ -1,11 +1,91 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"ares/database"
+	"ares/model"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"net/http"
+	"time"
+)
 
 // GetPostByID returns a single post object matching the provided ID
 func (controller *AresController) GetPostByID() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		id := ctx.Param("id")
+		requestAccountId := ctx.GetString("accountId")
 
+		_, err := primitive.ObjectIDFromHex(id)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "id is not a hex"})
+			return
+		}
+
+		post, err := database.FindDocumentById[model.Post](database.QueryParams{
+			MongoClient:    controller.DB,
+			DatabaseName:   controller.DatabaseName,
+			CollectionName: controller.CollectionName,
+		}, id)
+
+		if err != nil {
+			if err == mongo.ErrNoDocuments {
+				ctx.AbortWithStatus(http.StatusNotFound)
+				return
+			}
+
+			ctx.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		// TODO: Simplify this to not rely on string literal
+		authorAccount, err := database.FindDocumentById[model.Account](database.QueryParams{
+			MongoClient:    controller.DB,
+			DatabaseName:   controller.DatabaseName,
+			CollectionName: "account",
+		}, post.Author.Hex())
+
+		if err != nil {
+			if err == mongo.ErrNoDocuments {
+				ctx.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "author account not found"})
+				return
+			}
+
+			ctx.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		if authorAccount.Preferences.Privacy.ProfilePrivacy == model.PRIVATE {
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+			return
+		} else if authorAccount.Preferences.Privacy.ProfilePrivacy == model.FOLLOWER_ONLY {
+			reqAccountId, err := primitive.ObjectIDFromHex(requestAccountId)
+			if err != nil {
+				if err == mongo.ErrNoDocuments {
+					ctx.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "request account not found"})
+					return
+				}
+
+				ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "failed to perform requesting account lookup: " + err.Error()})
+				return
+			}
+
+			filter := bson.M{"followedId": authorAccount.ID, "followerId": reqAccountId}
+			_, err = database.FindDocumentByFilter[model.Follow](database.QueryParams{
+				MongoClient:    controller.DB,
+				DatabaseName:   controller.DatabaseName,
+				CollectionName: "follow",
+			}, filter)
+
+			if err != nil {
+				ctx.AbortWithStatus(http.StatusUnauthorized)
+				return
+			}
+		}
+
+		ctx.JSON(http.StatusOK, post)
 	}
 }
 
@@ -63,9 +143,67 @@ func (controller *AresController) GetLikeCount(key string) gin.HandlerFunc {
 //
 // If successful, a post ID will be returned with the document ID
 // in a success 200 OK response
-func (controller *AresController) CreatePost() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
+func (controller *AresController) CreatePost(s3Client *s3.Client, bucket string) gin.HandlerFunc {
+	type Params struct {
+		Location primitive.ObjectID  `json:"location,omitempty"`
+		Text     string              `json:"text,omitempty"`
+		Content  []model.ContentItem `json:"content" binding:"required"`
+		Tags     []string            `json:"tags,omitempty"`
+		Privacy  model.PrivacyLevel  `json:"privacy,omitempty"`
+	}
 
+	return func(ctx *gin.Context) {
+		var params Params
+		authorId := ctx.GetString("accountId")
+
+		err := ctx.ShouldBindJSON(&params)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "failed to bind params: " + err.Error()})
+			return
+		}
+
+		authorHex, err := primitive.ObjectIDFromHex(authorId)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "invalid author id hex: " + err.Error()})
+			return
+		}
+
+		if len(params.Content) > 10 {
+			ctx.AbortWithStatus(http.StatusRequestEntityTooLarge)
+			return
+		}
+
+		for _, content := range params.Content {
+			exists, err := database.Exists(s3Client, bucket, content.Destination)
+
+			if err != nil || !exists {
+				ctx.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "file does not exist in datalake"})
+				return
+			}
+		}
+
+		post := model.Post{
+			Author:    authorHex,
+			Location:  params.Location,
+			Text:      params.Text,
+			Content:   params.Content,
+			CreatedAt: time.Now(),
+			Tags:      params.Tags,
+			Privacy:   params.Privacy,
+		}
+
+		inserted, err := database.InsertOne[model.Post](database.QueryParams{
+			MongoClient:    controller.DB,
+			DatabaseName:   controller.DatabaseName,
+			CollectionName: controller.CollectionName,
+		}, post)
+
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "failed to insert document: " + err.Error()})
+			return
+		}
+
+		ctx.JSON(http.StatusCreated, gin.H{"message": inserted})
 	}
 }
 
