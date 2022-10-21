8a0c8b96b81961653c9d0aba8d8affd4d62a77ee
j-rockwell <john@llewkcor.com>
2022-10-21 13:26:29 -0700

    main: added s3 to ares controller

--- controller/controller.go
+++ controller/controller.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/go-redis/redis/v9"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -8,6 +9,7 @@ import (
 type AresController struct {
 	DB             *mongo.Client
 	RedisCache     *redis.Client
+	S3             *s3.Client
 	DatabaseName   string
 	CollectionName string
 }
